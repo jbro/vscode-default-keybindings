c072e37335e9e3826f403317b5359b5d70c6c61e
Jesper B. Rosenkilde <jbr@humppa.dk>
2024-08-19 14:40:02 +0200

    Mix in vs-code-default-keybindings for quicker release

--- scripts/build-keybindings.py
+++ scripts/build-keybindings.py
@@ -5,43 +5,43 @@ import re
 import requests
 
 defaultKeybindings = {
-  'config.vscode-default-keybindings.removeOSKeybindings && isLinux': 'https://raw.githubusercontent.com/jbro/vs-code-default-keybindings/master/linux.negative.keybindings.json',
-  'config.vscode-default-keybindings.removeOSKeybindings && isMac': 'https://raw.githubusercontent.com/jbro/vs-code-default-keybindings/master/macos.negative.keybindings.json',
-  'config.vscode-default-keybindings.removeOSKeybindings && isWindows': 'https://raw.githubusercontent.com/jbro/vs-code-default-keybindings/master/windows.negative.keybindings.json',
-  'config.vscode-default-keybindings.linuxKeybindings': 'https://raw.githubusercontent.com/jbro/vs-code-default-keybindings/master/linux.keybindings.json',
-  'config.vscode-default-keybindings.macOSKeybindings': 'https://raw.githubusercontent.com/jbro/vs-code-default-keybindings/master/macos.keybindings.json',
-  'config.vscode-default-keybindings.windowsKeybindings': 'https://raw.githubusercontent.com/jbro/vs-code-default-keybindings/master/windows.keybindings.json',
+  'config.vscode-default-keybindings.removeOSKeybindings && isLinux': 'vs-code-default-keybindings/linux.negative.keybindings.json',
+  'config.vscode-default-keybindings.removeOSKeybindings && isMac': 'vs-code-default-keybindings/macos.negative.keybindings.json',
+  'config.vscode-default-keybindings.removeOSKeybindings && isWindows': 'vs-code-default-keybindings/windows.negative.keybindings.json',
+  'config.vscode-default-keybindings.linuxKeybindings': 'vs-code-default-keybindings/linux.keybindings.json',
+  'config.vscode-default-keybindings.macOSKeybindings': 'vs-code-default-keybindings/macos.keybindings.json',
+  'config.vscode-default-keybindings.windowsKeybindings': 'vs-code-default-keybindings/windows.keybindings.json',
 }
 
 keybindings = []
 version = ''
 
-for expr, url in defaultKeybindings.items():
+for expr, path in defaultKeybindings.items():
 
-  # Download keybindings
-  response = requests.get(url)
-  response.raise_for_status()
-  
-  body = response.text
+  # Open keybindings file
+  with open(path, 'r') as f:
 
-  # Extract version from comments
-  comments = '\n'.join([ line for line in body.split('\n') if line.startswith('//') ])
-  version = re.search(r'\d+\.\d+\.\d+', comments).group(0)
+    # Read file
+    r = f.read()
 
-  # Remove comments
-  body = re.sub(r'//.*\n', '', body)
+    # Extract version from comments
+    comments = '\n'.join([ line for line in r.split('\n') if line.startswith('//') ])
+    version = re.search(r'\d+\.\d+\.\d+', comments).group(0)
 
-  # Parse keybindings
-  keymap = json.loads(body)
-  
-  # Update keybindings
-  for k in keymap:
-    if 'when' in k:
-      k['when'] = f'{expr} && ({k["when"]})'
-    else:
-      k['when'] = expr
-    
-    keybindings.append(k)
+    # Remove comments
+    r = re.sub(r'//.*\n', '', r)
+
+    # Parse keybindings
+    keymap = json.loads(r)
+
+    # Update keybindings
+    for k in keymap:
+      if 'when' in k:
+        k['when'] = f'{expr} && ({k["when"]})'
+      else:
+        k['when'] = expr
+
+      keybindings.append(k)
 
 # Update package.json
 with open('package.json', 'r+') as f:
